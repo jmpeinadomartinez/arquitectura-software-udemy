8564efe7d94958de68e82902b306b36e96afe29b
josemiguelpeinado <josemiguel.peinado@vasscompany.com>
2025-03-07 13:12:36 +0100

    Update to springboot 3.4.2 Added spring-boot-docker-compose 3.4.3 dependency to automatic execution of docker-compose to launch Postgre container Modify application.properties to execute automatic .sql and create new tables in ddbb Resolve incompatibilies between flyway and postgree versions lib Add Postman collection to test

--- cqrs/postgres/src/main/java/com/danielblanco/arquitecturasmodernas/cqrs/postgres/command/model/ReactionCommand.java
+++ cqrs/postgres/src/main/java/com/danielblanco/arquitecturasmodernas/cqrs/postgres/command/model/ReactionCommand.java
@@ -1,11 +1,11 @@
 package com.danielblanco.arquitecturasmodernas.cqrs.postgres.command.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "comment_reaction", schema = "cqrs")
